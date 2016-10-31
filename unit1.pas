09ea48d7dd2c67f65697e5eba60128ad197ffd3f
Arina Ivanova <arishkame@gmail.com>
2016-10-31 23:51:02 +0300

    Корень и 4 действия

--- unit1.pas
+++ unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls, Math;
 
 type
 
@@ -13,28 +13,18 @@ type
 
   TForm1 = class(TForm)
     add_button: TButton;
+    sqrt_button: TButton;
     div_button: TButton;
-    Edit7: TEdit;
-    Edit8: TEdit;
-    Label7: TLabel;
-    Label8: TLabel;
     myl_button: TButton;
-    Edit5: TEdit;
-    Edit6: TEdit;
-    Label5: TLabel;
-    Label6: TLabel;
     sub_button: TButton;
     Edit1: TEdit;
     Edit2: TEdit;
-    Edit3: TEdit;
-    Edit4: TEdit;
     Label1: TLabel;
     Label2: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
     procedure add_buttonClick(Sender: TObject);
     procedure div_buttonClick(Sender: TObject);
     procedure myl_buttonClick(Sender: TObject);
+    procedure sqrt_buttonClick(Sender: TObject);
     procedure sub_buttonClick(Sender: TObject);
     procedure Edit1Change(Sender: TObject);
     procedure Label1Click(Sender: TObject);
@@ -60,17 +50,22 @@ end;
 
 procedure TForm1.div_buttonClick(Sender: TObject);
 begin
-     label8.Caption := FloatToStr(StrToFloat(edit7.Text) / StrToFloat(edit8.Text));
+      label1.Caption := FloatToStr(StrToFloat(edit1.Text) / StrToFloat(edit2.Text));
 end;
 
 procedure TForm1.myl_buttonClick(Sender: TObject);
 begin
-      label6.Caption := FloatToStr(StrToFloat(edit5.Text) * StrToFloat(edit6.Text));
+      label1.Caption := FloatToStr(StrToFloat(edit1.Text) * StrToFloat(edit2.Text));
+end;
+
+procedure TForm1.sqrt_buttonClick(Sender: TObject);
+begin
+          label1.Caption := FloatToStr(sqrt(StrToFloat(edit1.Text)));
 end;
 
 procedure TForm1.sub_buttonClick(Sender: TObject);
 begin
-      label4.Caption := FloatToStr(StrToFloat(edit3.Text) - StrToFloat(edit4.Text));
+      label1.Caption := FloatToStr(StrToFloat(edit1.Text) - StrToFloat(edit2.Text));
 end;
 
 procedure TForm1.Edit1Change(Sender: TObject);
