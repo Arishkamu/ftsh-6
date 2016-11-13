9663d73faee3bcccc84f85ad94f364c581e67ac6
Arina Ivanova <arishkame@gmail.com>
2016-11-13 21:56:30 +0300

    ячейки памяти

--- unit1.pas
+++ unit1.pas
@@ -13,6 +13,13 @@ type
 
   TForm1 = class(TForm)
     add_button: TButton;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
     sqrt_button: TButton;
     div_button: TButton;
     myl_button: TButton;
@@ -22,6 +29,12 @@ type
     Label1: TLabel;
     Label2: TLabel;
     procedure add_buttonClick(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
     procedure div_buttonClick(Sender: TObject);
     procedure myl_buttonClick(Sender: TObject);
     procedure sqrt_buttonClick(Sender: TObject);
@@ -36,7 +49,7 @@ type
 
 var
   Form1: TForm1;
-
+  a:Real;
 implementation
 
 {$R *.lfm}
@@ -48,9 +61,39 @@ begin
      label1.Caption := FloatToStr(StrToFloat(edit1.Text) + StrToFloat(edit2.Text));
 end;
 
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+    a:= StrToFloat(label1.Caption);
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+     a:= StrToFloat(edit2.Text);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+     a:= StrToFloat(edit1.Text);
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+    label1.Caption := FloatToStr( 1/ (StrToFloat(edit1.Text)));
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+begin
+
+end;
+
 procedure TForm1.div_buttonClick(Sender: TObject);
 begin
-      label1.Caption := FloatToStr(StrToFloat(edit1.Text) / StrToFloat(edit2.Text));
+     label1.Caption := FloatToStr(StrToFloat(edit1.Text) / StrToFloat(edit2.Text));
 end;
 
 procedure TForm1.myl_buttonClick(Sender: TObject);
